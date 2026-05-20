4c4612d0c6bba7db68e6e3052b6349502606ec3a
TT <tt@secblog.dev>
2026-05-21 02:10:08 +0800

    feat: 恢复音乐播放器组件

---
+++ 520-memorial/src/config.ts
@@ -0,0 +1,15 @@
+/**
+ * 网站配置文件
+ * 修改此处可自定义网站内容
+ */
+export const config = {
+  // 恋爱开始日期（格式：YYYY-MM-DD）
+  startDate: "2025-09-28",
+  // 两人名字
+  name1: "TT",
+  name2: "CC",
+  // 背景音乐 URL
+  musicUrl: "/music/bg.mp3",
+  // 页面标题
+  pageTitle: "TT & CC - Our Love Story",
+};
